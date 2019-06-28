86bdac27bef43928b4d2855ffaa2e32214eaa7bf
Rahul Raut <rahulraut.1588@gmail.com>
2019-06-29 03:17:11 +0530

    Imported Router Module and implemented path configuration for contact list page

--- con-list/src/app/app.module.ts
+++ con-list/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { ContactDataService } from './common/contact-data.service';
 import { ContactlistComponent } from './contactlist/contactlist.component';
+
+const myRoutes = [
+  { path: '', redirectTo:'/contactList', pathMatch:'full' },
+  { path: 'contactList', component: ContactlistComponent }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { ContactlistComponent } from './contactlist/contactlist.component';
     ContactlistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [ ContactDataService ], 
   bootstrap: [ AppComponent ]

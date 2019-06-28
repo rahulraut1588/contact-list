31e8a7b36fe6564f199ebaf1d976612dff81a03c
Rahul Raut <rahulraut.1588@gmail.com>
2019-06-29 03:09:49 +0530

    Created Contact List Component and retrieved data from service file

--- con-list/src/app/app.module.ts
+++ con-list/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { ContactDataService } from './common/contact-data.service';
+import { ContactlistComponent } from './contactlist/contactlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactlistComponent
   ],
   imports: [
     BrowserModule
---
+++ con-list/src/app/contactlist/contactlist.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ContactDataService } from '../common/contact-data.service';
+
+@Component({
+    selector: 'my-contact-list',
+    templateUrl: './contactlist.component.html'
+})
+
+export class ContactlistComponent {
+    
+    list:any;
+
+    constructor( private conList:ContactDataService ) {
+        this.list = conList.getContactList();
+    }
+}
